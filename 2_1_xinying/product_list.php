5e6e75f76ca6add67f44ada09886ff2da7a63234
xinyingren <xinyingren@users.noreply.github.com>
2021-04-04 19:57:09 -0400

    7_1_4/4/2021 update

--- 2_1_xinying/product_list.php
+++ 2_1_xinying/product_list.php
@@ -4,7 +4,14 @@
 	<meta charset="UTF-8">
 	<title>Product List</title>
 
-	<?php include "parts/meta.php"; ?>
+	
+
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" type="text/css" href="css/storetheme.css">
+	<link rel="stylesheet" href="https://use.typekit.net/arl3erg.css">
 </head>
 <body>
 
