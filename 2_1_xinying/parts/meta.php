45595b208c78396ce1c604f6b681953b41aada17
xinyingren <xinyingren@users.noreply.github.com>
2021-04-04 18:52:53 -0400

    rename

---
+++ 2_1_xinying/parts/meta.php
@@ -0,0 +1,9 @@
+
+
+
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" type="text/css" href="css/storetheme.css">
+	<link rel="stylesheet" href="https://use.typekit.net/arl3erg.css">
---
+++ 2_1_xinying/parts/navbar.php
@@ -0,0 +1,18 @@
+
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Peppercron Kitchen</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+				<ul>
+					<!-- li*3>a[href=#]>{Link $} -->
+					<li><a href="index.html">HOME</a></li>
+					<li><a href="product_list.php">MENU</a></li>
+					<li><a href="#">OUR STORY</a></li>
+					<li><a href="#">CONTACT US</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
