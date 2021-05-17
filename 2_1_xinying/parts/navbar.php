06237ae6f6ebcae6a67216ee50a3ca582542833c
xinyingren <xinyingren@users.noreply.github.com>
2021-05-16 22:23:05 -0400

    5/16

--- 2_1_xinying/parts/navbar.php
+++ 2_1_xinying/parts/navbar.php
@@ -8,7 +8,7 @@
 			<nav class="nav nav-flex flex-none">
 				<ul>
 					<!-- li*3>a[href=#]>{Link $} -->
-					<li><a href="index.html">HOME</a></li>
+					<li><a href="index.php">HOME</a></li>
 					<li><a href="product_list.php">MENU</a></li>
 					<li><a href="#">OUR STORY</a></li>
 					<li><a href="#">CONTACT US</a></li>
---
+++ 2_1_xinying/product_checkout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Checkout Page</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Product Checkout</h2>
+			<p>What do you got in there?</p>
+			<p><a href="product_checkout.php">Checkout</a></p>
+		</div>
+	</div>
+	
+</body>
+</html>
